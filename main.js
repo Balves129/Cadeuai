0091d0fcdd8f0c2e0a13493ac5c1ff9b95a512f2
Balves129 <b.balves129@gmail.com>
2023-03-30 16:21:05 -0300

    projeto-CadeUai

--- main.js
+++ main.js
@@ -11,14 +11,14 @@ const text = document.querySelector(".sec-text");
 
     const textLoad = () => {
       setTimeout(() => {
-          text.textContent = "facilidade"; //Aqui adiciona um texto dentro do content do Css
+          text.textContent = "Facilidade"; //Aqui adiciona um texto dentro do content do Css
       }, 0);
       setTimeout(() => { //setTimeout controla o tempo da animação em milissegundos
           text.textContent = "Conforto";
-      }, 4000);
+      }, 4500);
       setTimeout(() => {
           text.textContent = "Precisão";
-      }, 8000);
+      }, 8650);
     }
     
 
@@ -26,15 +26,24 @@ const text = document.querySelector(".sec-text");
     setInterval(textLoad, 12000);
 
     //Carrossel 
-    let images = Array.from(document.getElementsByClassName('imgCarousel')) //Aqui fazemos a busca do objeto pela sua classe Html
+    // let images = Array.from(document.getElementsByClassName('imgCarousel')) //Aqui fazemos a busca do objeto pela sua classe Html
 
-    let mainPhoto = document.getElementById("mainPhoto")
+    // let mainPhoto = document.getElementById("mainPhoto")
 
-    function updateImage(event){
-      let image = event.target
+    // function updateImage(event){
+    //   let image = event.target
 
-      mainPhoto.src = image.src
-    }
-    images.forEach(function(image){
-      image.addEventListener("click",updateImage)
+    //   mainPhoto.src = image.src
+    // }
+    // images.forEach(function(image){
+    //   image.addEventListener("click",updateImage)
+    // });
+    const accordionHeaders = document.querySelectorAll('.accordion-header');
+
+    accordionHeaders.forEach(header => {
+      header.addEventListener('click', () => {
+        const accordionItem = header.parentElement;
+        const accordionContent = accordionItem.querySelector('.accordion-content');
+        accordionContent.classList.toggle('active');
+      });
     });
